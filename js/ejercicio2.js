4fa7352eacc6b50ccab98e095885f60a6eee31d8
Jose Antonio Pozo Gonzalez <iwc70842@educastur.es>
2024-12-26 09:03:47 +0100

    añadir numeros ramdom y comienzo de comprueba casilla sin funcionar

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -5,26 +5,53 @@
 * @description  Funciones para Generar el tablero de Sudoku y mostrar en funcion de la dificultad un número determinado de casillas. 
 */
 
-var sudoku =[];
+var sudoku = [];
+var esValida = false;
 
 //Llenamos la matriz de ceros
-for (let i =0;i<9;i++){
-  sudoku[i]=[];
-  for (let j=0;j<9;j++){
-    sudoku[i][j]=0;
+for (let i = 0; i < 9; i++) {
+  sudoku[i] = [];
+  for (let j = 0; j < 9; j++) {
+    sudoku[i][j] = 0;
   }
 }
 // muestra el sudoku 
-for (let i=0;i<9;i++){
-  for(let j=0;j<9;j++){
-    document.getElementById(i+","+j).innerText=sudoku[i][j];
+for (let i = 0; i < 9; i++) {
+  for (let j = 0; j < 9; j++) {
+    document.getElementById(i + "," + j).innerText = sudoku[i][j];
   }
- 
 }
 
-function rellenaSudoku(){
-  // generar un numero aleatorio para rellenar una casilla
-let valor=Math.floor(Math.random()*9)+1
+rellenaSudoku();
 
+function rellenaSudoku() {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (sudoku[i][j] === 0) {
+        while (!esValida) {
+          let valor = Math.floor(Math.random() * 9) + 1
+          document.getElementById(i + "," + j).innerText = valor;
+          compruebaCasilla(i, j, valor)
+        }
+        
+      }
+      esValida = false;
+    }
+  }
+}
 
+function compruebaCasilla(fila, columna, valor) {
+  //comprobación fila
+  for (let i = 0; i < 9; i++) {
+    if (i===columna) continue;
+    if (valor === sudoku[fila][i]) {
+      return;
+    }else{esValida= true;}
+  }
+
+  //comprobación columna
+
+  //comprobación cuadrante 3x3
+
+  //esValida = true;
 }
